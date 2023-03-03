d173b793a8cbc544de5f3fa07288802bf0a0e54b
bennozik <bennozik68@gmail.com>
2023-03-03 12:57:10 -0500

    Implement draft of WindModel Class

---
+++ rrga/core/src/com/mygdx/game/WindModel.java
@@ -0,0 +1,53 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.JsonValue;
+import com.mygdx.game.obstacle.*;
+
+
+/**
+ * A model for wind objects.
+ * Currently extends PolygonObstacle to allow for different shaped wind gusts, but may want to change later
+ * to make drawing manageable/easier
+ */
+public class WindModel extends PolygonObstacle {
+    /**
+     * The initializing data (to avoid magic numbers)
+     */
+    private final JsonValue data;
+
+    /**
+     * The magnitude of this wind's force
+     * Probably going to want to add some invariant
+     */
+    private float magnitude;
+
+    /**
+     * The direction of this wind's force in radians. Value within [0, 2pi).
+     */
+    private float direction;
+
+    public WindModel(JsonValue data) {
+        super(data.get("points").asFloatArray(), data.getFloat("x"), data.getFloat("y"));
+        magnitude = data.getFloat("magnitude");
+        direction = data.getFloat("direction");
+        this.data = data;
+    }
+
+    /**
+     * Returns a value between 0 and 1 which gives the magnitude of the force on the umbrella from the wind
+     */
+    public float getWindForce(float umbrellaAngle){
+        //may need to change the umbrella angle based up the value returned by umbrella.getRotation.
+        //for now assuming value is within[0, 2pi).
+        float windx = (float) Math.cos(direction);
+        float windy = (float) Math.sin(direction);
+        float umbrellax = (float) Math.cos(umbrellaAngle);
+        float umbrellay = (float) Math.sin(umbrellaAngle);
+        float dot = Vector2.dot(windx, windy, umbrellax, umbrellay);
+        if (dot<0) return 0;
+        else return dot;
+    }
+
+
+}
