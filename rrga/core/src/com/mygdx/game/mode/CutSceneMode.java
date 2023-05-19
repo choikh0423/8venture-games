a7ab1ca8eb369e7aefedaa3624fbd1b5bd88715f
Kyu Hwan Choi <57926472+choikh0423@users.noreply.github.com>
2023-05-18 20:26:19 -0400

    Added Cutscene assets + Skeleton for cutscenemode

---
+++ rrga/core/src/com/mygdx/game/mode/CutSceneMode.java
@@ -0,0 +1,256 @@
+package com.mygdx.game.mode;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Cursor;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.CameraController;
+import com.mygdx.game.GameCanvas;
+import com.mygdx.game.GameMode;
+import com.mygdx.game.screen.MenuScreen;
+import com.mygdx.game.utility.assets.AssetDirectory;
+import com.mygdx.game.utility.util.ScreenListener;
+
+/**
+ * A PauseMode is a pause menu screen. User can interact with this screen
+ * through keyboard or mouse cursor.
+ */
+public class CutSceneMode extends MenuScreen {
+
+    /** Listener that will update the player mode when we are done */
+    private ScreenListener listener;
+
+    /** The Screen to draw underneath the pause screen*/
+    private GameMode gameScreen;
+
+    /** Reference to GameCanvas created by the root */
+    protected GameCanvas canvas;
+
+    /** overlay texture */
+    private TextureRegion foregroundTexture;
+
+    /** The background tinting color cache */
+    private Color overlayTint;
+
+    /** A reference to a text font (changes to any of its properties will be global) */
+    private BitmapFont bigFont;
+
+    private BitmapFont smallFont;
+
+    /** exit code to toggle pause state */
+    public static final int EXIT_RESUME = 1;
+
+    /** exit code to restart game */
+    public static final int EXIT_RESTART = 2;
+    /** exit code to restart game */
+    public static final int EXIT_MENU = 3;
+    public static final int EXIT_SETTINGS = 4;
+
+    /** current assigned exit code of mode (valid exits are non-negative) */
+    private int currentExitCode;
+
+
+    /** The current state of the level menu button */
+    private int menuPressState;
+    /** The current state of the restart button */
+    private int restartPressState;
+    /** The current state of the back button */
+    private int backPressState;
+    private int settingsPressState;
+
+    /** exit button*/
+    private MenuButton menuButton;
+    /** start button */
+    private MenuButton restartButton;
+    /** back button */
+    private MenuButton backButton;
+    /** settings button */
+    private MenuButton settingsButton;
+
+    /** Height of the button */
+    private static float BUTTON_SCALE  = 1.0f;
+    /** Touch range constant */
+    private static float TOUCH_AREA_RATIO = 0.95f;
+    private float TAG_SCL = 1;
+    /** Scaling factor for when the player changes the resolution. */
+    private float scale;
+
+    /** Standard window size (for scaling) */
+    private static int STANDARD_WIDTH  = 1024;
+    /** Standard window height (for scaling) */
+    private static int STANDARD_HEIGHT = 576;
+
+    /** Pause text related variables */
+    private TextureRegion skipTag;
+    private static float SKIP_X_RATIO = .5f;
+    private static float SKIP_Y_RATIO = .65f;
+    private int skipTagX;
+    private int skipTagY;
+    /** Texture for the cursor */
+    private TextureRegion cursorTexture;
+    /** true until the first call to render*/
+    public boolean first;
+
+    public CutSceneMode(GameCanvas canvas) {
+        this.canvas = canvas;
+        overlayTint = new Color(1,1,1,0.9f);
+        currentExitCode = Integer.MIN_VALUE;
+        first = true;
+
+        int width = (int) canvas.getCamera().getViewWidth();
+        int height = (int) canvas.getCamera().getViewHeight();
+        float sx = ((float)width)/STANDARD_WIDTH;
+        float sy = ((float)height)/STANDARD_HEIGHT;
+        scale = Math.min(sx, sy);
+
+        menuButton.setPos(width, height, scale);
+        restartButton.setPos(width, height, scale);
+        backButton.setPos(width, height, scale);
+        settingsButton.setPos(width, height, scale);
+
+        skipTagY = (int)(SKIP_Y_RATIO * height);
+        skipTagX = (int)(SKIP_X_RATIO * width);
+    }
+
+    /**
+     * Gather the assets for this controller.
+     * This method extracts the asset variables from the given asset directory. It
+     * should only be called after the asset directory is completed.
+     *
+     * @param directory    Reference to global asset manager.
+     */
+    public void gatherAssets(AssetDirectory directory) {
+        //TODO: texture is unnecessary, use shapes (see prof White's lectures on drawing shapes without textures)
+        foregroundTexture = new TextureRegion(directory.getEntry( "menu:background2", Texture.class ));
+
+        TextureRegion menuTexture = new TextureRegion(directory.getEntry("menu:menu_button", Texture.class));
+        TextureRegion restartTexture = new TextureRegion(directory.getEntry("menu:restart_button", Texture.class));
+        TextureRegion backButtonTexture = new TextureRegion(directory.getEntry("menu:back_button", Texture.class));
+        TextureRegion settingsTexture = new TextureRegion(directory.getEntry("menu:settings_button", Texture.class));
+
+//        pauseTag = new TextureRegion(directory.getEntry("pause:pause_tag", Texture.class));
+
+        cursorTexture = new TextureRegion(directory.getEntry("menu:cursor_menu", Texture.class));
+
+        menuButton.setTexture(menuTexture);
+        restartButton.setTexture(restartTexture);
+        backButton.setTexture(backButtonTexture);
+        settingsButton.setTexture(settingsTexture);
+    }
+
+    /**
+     * Draw the Pause menu and exit pause mode if possible.
+     * @param delta The time in seconds since the last render.
+     */
+    public void render(float delta) {
+//        if (background != null){
+//            background.render(delta);
+//        }
+
+        //Gdx.input.setCursorCatched(false);
+//        int x=0, y=0;
+//        if(first) {
+//            x = Gdx.input.getX();
+//            y = Gdx.input.getY();
+//        }
+//        Gdx.graphics.setSystemCursor(Cursor.SystemCursor.None);
+//        if(first){
+//            Gdx.input.setCursorPosition(x, y);
+//            first = false;
+//        }
+
+        //gameScreen.draw(delta);
+        draw(delta);
+    }
+
+    /**
+     * Draws static pause menu
+     * @param delta The time in seconds since the last render
+     */
+    private void draw(float delta){
+        canvas.begin();
+        canvas.draw(foregroundTexture, overlayTint, 0, 0, canvas.getWidth(), canvas.getHeight());
+
+
+        canvas.draw(pauseTag, Color.WHITE, pauseTag.getRegionWidth()/2f, pauseTag.getRegionHeight()/2f,
+                pauseTagX, pauseTagY, 0 , TAG_SCL * scale, TAG_SCL * scale);
+
+        menuButton.draw(canvas, menuPressState, BUTTON_SCALE, Color.WHITE);
+        restartButton.draw(canvas, restartPressState, BUTTON_SCALE, Color.WHITE);
+        backButton.draw(canvas, backPressState, BUTTON_SCALE, Color.WHITE);
+        settingsButton.draw(canvas, settingsPressState, BUTTON_SCALE, Color.WHITE);
+
+        CameraController camera = canvas.getCamera();
+        //draw mouse texture
+        int mx = Gdx.input.getX();
+        int my = Gdx.input.getY();
+        // retrieve the viewport coordinate to draw cursor
+        Vector2 pos = camera.unproject(mx, my);
+        if(pos.x <= camera.getViewWidth() && pos.x>= 0 && pos.y < camera.getViewHeight() && pos.y >0) {
+            canvas.draw(cursorTexture, Color.WHITE, 0, cursorTexture.getRegionHeight(),
+                    pos.x, pos.y, 0, .4f, .4f);
+            Gdx.graphics.setSystemCursor(Cursor.SystemCursor.None);
+        }
+        else {
+            Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Arrow);
+        }
+
+        canvas.end();
+    }
+
+    public void resize(int width, int height) {
+        // resizing done through viewport
+    }
+
+    public void dispose() {
+        listener = null;
+        gameScreen = null;
+        canvas = null;
+        foregroundTexture = null;
+        overlayTint = null;
+        bigFont = null;
+        smallFont = null;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if (keycode == Input.Keys.SPACE){
+            currentExitCode = EXIT_RESUME;
+        }
+        return false;
+    }
+    @Override
+    public boolean keyUp(int keycode) {
+        if (currentExitCode > 0) {
+            listener.exitScreen(this, currentExitCode);
+            currentExitCode = Integer.MIN_VALUE;
+        }
+
+        return false;
+    }
+
+    /**
+     * Sets the ScreenListener for this mode.
+     * The ScreenListener will respond to requests to quit.
+     */
+    public void setScreenListener(ScreenListener listener){
+        this.listener = listener;
+    }
+
+    public void setBackgroundScreen(GameMode gameScreen){
+        this.gameScreen = gameScreen;
+    }
+
+    public GameMode getBackgroundScreen(){
+        return this.gameScreen;
+    }
+
+    public void reset() {
+        overlayTint = new Color(1,1,1,0.9f);
+        currentExitCode = Integer.MIN_VALUE;
+    }
+}
