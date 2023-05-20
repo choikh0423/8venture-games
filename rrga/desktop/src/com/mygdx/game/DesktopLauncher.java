e8f450ec956f6d78226b1994a3d27e931720c624
Kyu Hwan Choi <57926472+choikh0423@users.noreply.github.com>
2023-05-20 03:02:24 -0400

    Remove tile extension

--- rrga/desktop/src/com/mygdx/game/DesktopLauncher.java
+++ rrga/desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -8,17 +8,13 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setTitle("Gale - 8venture Games");
+		config.setTitle("Gale");
 		config.setResizable(true);
 		// Default size for Physics Lab 4
 		config.setWindowedMode(1024, 576);
-		if (arg.length > 0) {
-			new Lwjgl3Application(new GDXRoot(arg[0]), config);
-		}
-		else {
-			// standard, use this branch for shipping
-			new Lwjgl3Application(new GDXRoot(), config);
-		}
+
+		// standard, use this branch for shipping
+		new Lwjgl3Application(new GDXRoot(), config);
 
 	}
 }
